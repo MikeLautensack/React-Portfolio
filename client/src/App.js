fb815a50ba5ff707629d96f4d6e5e49b9af9c3ce
Mike <mikelautensack100@gmail.com>
2023-01-28 00:26:58 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import Navbar from './components/Navbar';
+import Home from './components/Home'
+import About from './components/About';
+import Skills from './components/Skills';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
